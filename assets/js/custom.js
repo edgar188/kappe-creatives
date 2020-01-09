77adab1ed3d7364c2816c4f1db30853c0579862c
edgar <edgar.hovhannisyan08@gmail.com>
2020-01-09 19:40:33 +0400

    New bug fixes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,3 +5,9 @@ function onInfo(target) {
         ? hiddenNode.setAttribute("style", "display: block; visibility: visible;")
         : hiddenNode.setAttribute("style", "display: none; visibility: hidden;");
 }
+
+setInterval(() => {
+    if(window.innerWidth > 768) {
+        document.getElementById("navs").setAttribute("style", "max-width: 100%;height 100%; opacity: 1")
+    }
+}, 1000);
